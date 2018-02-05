231d8a463c5c3e2d4f1495ceeae35e858f913d3e
Tsuna-Cheung <237971264@qq.com>
2018-02-05 10:26:53 +0000

    2

--- www/js/index.js
+++ www/js/index.js
@@ -1,44 +1,19 @@
-var paused_count =0;
-var resumed_count = 0;
-var launched_count = 0;
+Backendless.initApp("B7472B45-590F-C195-FFB7-82A61F737500","86BE4E33-1AA6-B927-FF13-350293F5D200");
 
-document.addEventListener("deviceready", onDeviceReady, false);
-		
-	
-function updateDisplay() {
-	$("#launched").text("Application launched: " + launched_count);
-	$("#resumed").text("Application paused: " + paused_count);
-	$("#paused").text("Application resumed: " + resumed_count);
+$(document).on("pageshow","#todoPage", onPageShow);
+
+function onPageShow() {
+	console.log("page shown");
+} 
+
+Backendless.Data.of("TASKS").find().then(processResults).catch(error);
+
+function processResults(tasks) {
+   //display the first task in an array of tasks. 
+alert(tasks[0].Task)
 }
 
+function error(err) {
+    alert(err);
+}
 
-// device APIs are available
-//
-    function onDeviceReady() {
-	
-	document.addEventListener("resume", onResume, false);
-	document.addEventListener("pause", onPause, false);
-	
-	launched_count++;
-	updateDisplay();
-	    
-	alert("device ready");
-    }
-
-
-    function onPause() {
-	
-	paused_count++;
-	updateDisplay();
-	    
-	alert("pause");
-    }
-	
-
-    function onResume() {
-		
-	resumed_count++;
-	updateDisplay();
-	    
-	alert("resume");
-    }
